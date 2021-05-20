a49d7d60be8bde89c9445a62bd7798b5395be256
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-05-20 16:22:38 +0700

    Init before useMemo boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useState } from "react";
+
 function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = slowFunction(number);
+
+  const theme = {
+    backgroundColor: dark ? "#333" : "#FFF",
+    color: dark ? "#FFF" : "#333",
+  };
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+
+      <div style={theme}>{doubleNumber}</div>
+    </>
   );
+}
+
+function slowFunction(num) {
+  console.log("Calling slow function...");
+
+  for (let i = 0; i <= 1000000000; i++) {}
+  
+  return num * 2;
 }
 
 export default App;
